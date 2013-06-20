c11bc1fa2a777d457d4de398b569006d2b8820f5
iiTsai <470597142@qq.com>
2013-06-20 22:43:54 +0800

    更新colorInverse方法,修正BUG

--- src/iTsai-webtools.core.js
+++ src/iTsai-webtools.core.js
@@ -19,8 +19,8 @@
  * osc: http://my.oschina.net/tsai
  * @method ()
  */
-(function() {
-	if (!window.iTsai){
+(function () {
+    if (!window.iTsai) {
         window.iTsai = {};
     }
 })();
@@ -30,178 +30,182 @@
  * @module iTsai
  */
 iTsai = {
-	/**
-	 * iTsai库版本
+    /**
+     * iTsai库版本
      * @type {String}
      * @property version
-	 */
-	version : '0.2.9',
-	/**
-	 * 显示当前对象名称路径
-	 * 
-	 * @method toString
-	 * @return {String} 'iTsai'
-	 */
-	toString : function() {
-		return 'iTsai';
-	},
-	/**
-	 * 生成唯一CID编号:时间+4位随机数
-	 * 
-	 * @method random
-	 * @return {String} 随机数
-	 */
-	random : function() {
-		return new Date().getTime() + '' + Math.round(Math.random() * 10000);
-	},
-	/**
-	 * 判断是否含有'.'号
-	 * 
-	 * @method hasDot
-	 * @param {String} str 输入字符串
-	 * @return {Boolean}
-	 */
-	hasDot : function(str) {
-		if (typeof str != 'string') {
-			return false;
-		}
-		if (str.indexOf('.') != -1) {
-			return true;
-		}
-		return false;
-	},
-	/**
-	 * 判断对象是否为纯整形数字或整形数字字符串 011=9(011 表示8进制)
-	 * 
-	 * @method isInteger
-	 * @param {Number/String} obj 输入数字或字符串
-	 * @return {Boolean}
-	 */
-	isInteger : function(obj) {
-		if (obj != parseInt(obj, 10)) {
-			return false;
-		}
-		return true;
-	},
-	/**
-	 * 将"undefined"和null转换为空串
-	 * 
-	 * @method obj2Empty
-	 * @param {Object} obj 输入对象
-	 * @return {Object}
-	 */
-	obj2Empty : function(obj) {
-		if (typeof obj == "undefined" || obj == null) {
-			return '';
-		}
-		return obj;
-	},
-	/**
-	 * 检测插件是否存在,如:'Quicktime'/'Quicktime.Quicktime'<br>
-	 * IE浏览器控件的名称通道和其它浏览器插件名称不一致
-	 * 
-	 * @method checkPlugin
-	 * @param {String} name 插件名称
-	 * @param {String} nameIE [optional,default=name] IE浏览器ActiveX插件名称
-	 * @return {Boolean} true-插件已经安装;false-未安装
-	 */
-	checkPlugin : function(name, nameIE) {
-		var ie = '';
-		if (typeof nameIE === 'undefined') {
-			ie = name;
-		} else {
-			ie = nameIE;
-		}
-		return this.hasPluginIE(ie) || this.hasPlugin(name);
-	},
-	/**
-	 * 检测非IE浏览器插件是否存在
-	 * 
-	 * @method hasPlugin
-	 * @param {String} name 插件名称
-	 * @return {Boolean} true-插件已经安装;false-未安装
-	 */
-	hasPlugin : function(name) {
-		if (!name)
-			return false;
-		name = name.toLowerCase();
-		var plugins = window.navigator.plugins;
-		for ( var i = 0; i < plugins.length; i++) {
-			if (plugins[i] && plugins[i].name.toLowerCase().indexOf(name) > -1) {
-				return true;
-			}
-		}
-		return false;
-	},
-	/**
-	 * 检测IE浏览器插件是否存在
-	 * 
-	 * @method hasPluginIE
-	 * @param {String} name IE浏览器ActiveX插件名称
-	 * @return {Boolean} true-插件已经安装;false-未安装
-	 */
-	hasPluginIE : function(name) {
-		if (!name)
-			return false;
-		try {
-			new ActiveXObject(name);
-			return true;
-		} catch (ex) {
-			return false;
-		}
-	},
-	/**
-	 * 颜色取反，如将白色'#ffffff'转换为黑色'#000000'
-	 * 
-	 * @method colorInverse
-	 * @param {String} color 颜色16进制字符表示形式，如：'#ff0000'，表示红色。
-	 * @return {String} 取反后的颜色
-	 */
-	colorInverse : function(color) {
-		color = !color ? '' : color;
-		color = parseInt(color.replace('#', '0x'));
-		var r = color >> 16, g = color >> 8 & 0x0000ff, b = color & 0x0000ff, _r = 255 - r, _g = 255 - g, _b = 255 - b, clr = '#'
-				+ (_r << 16 | _g << 8 | _b).toString(16);
-		return clr == '#0' ? '#000000' : clr;
-	},
-	/**
-	 * 获取浏览器语言代码,如:'zh-CN'
-	 * 
-	 * @method getLang
-	 * @return {String} 语言代码
-	 */
-	getLang : function() {
-		var nav = window.navigator;
-		return (nav.language || nav.userLanguage);
-	},
-	/**
-	 * 取消事件冒泡
-	 * 
-	 * @method stopBubble
-	 * @param {Object} e 事件对象
-	 */
-	stopBubble : function(e) {
-		if (e && e.stopPropagation) {
-			e.stopPropagation();
-		} else {
-			// ie
-			window.event.cancelBubble = true;
-		}
-	},
-	/**
-	 * 阻止浏览器默认行为
-	 * 
-	 * @method stopDefault
-	 * @param {Object} e 事件对象
-	 * @return {Boolean}
-	 */
-	preventDefault : function(e) {
-		if (e && e.preventDefault) {
-			e.preventDefault();
-		} else {
-			// ie
-			window.event.returnValue = false;
-		}
-		return false;
-	}
+     */
+    version: '0.2.9',
+    /**
+     * 显示当前对象名称路径
+     *
+     * @method toString
+     * @return {String} 'iTsai'
+     */
+    toString: function () {
+        return 'iTsai';
+    },
+    /**
+     * 生成唯一CID编号:时间+4位随机数
+     *
+     * @method random
+     * @return {String} 随机数
+     */
+    random: function () {
+        return new Date().getTime() + '' + Math.round(Math.random() * 10000);
+    },
+    /**
+     * 判断是否含有'.'号
+     *
+     * @method hasDot
+     * @param {String} str 输入字符串
+     * @return {Boolean}
+     */
+    hasDot: function (str) {
+        if (typeof str != 'string') {
+            return false;
+        }
+        if (str.indexOf('.') != -1) {
+            return true;
+        }
+        return false;
+    },
+    /**
+     * 判断对象是否为纯整形数字或整形数字字符串 011=9(011 表示8进制)
+     *
+     * @method isInteger
+     * @param {Number/String} obj 输入数字或字符串
+     * @return {Boolean}
+     */
+    isInteger: function (obj) {
+        if (obj != parseInt(obj, 10)) {
+            return false;
+        }
+        return true;
+    },
+    /**
+     * 将"undefined"和null转换为空串
+     *
+     * @method obj2Empty
+     * @param {Object} obj 输入对象
+     * @return {Object}
+     */
+    obj2Empty: function (obj) {
+        if (typeof obj == "undefined" || obj == null) {
+            return '';
+        }
+        return obj;
+    },
+    /**
+     * 检测插件是否存在,如:'Quicktime'/'Quicktime.Quicktime'<br>
+     * IE浏览器控件的名称通道和其它浏览器插件名称不一致
+     *
+     * @method checkPlugin
+     * @param {String} name 插件名称
+     * @param {String} nameIE [optional,default=name] IE浏览器ActiveX插件名称
+     * @return {Boolean} true-插件已经安装;false-未安装
+     */
+    checkPlugin: function (name, nameIE) {
+        var ie = '';
+        if (typeof nameIE === 'undefined') {
+            ie = name;
+        } else {
+            ie = nameIE;
+        }
+        return this.hasPluginIE(ie) || this.hasPlugin(name);
+    },
+    /**
+     * 检测非IE浏览器插件是否存在
+     *
+     * @method hasPlugin
+     * @param {String} name 插件名称
+     * @return {Boolean} true-插件已经安装;false-未安装
+     */
+    hasPlugin: function (name) {
+        if (!name)
+            return false;
+        name = name.toLowerCase();
+        var plugins = window.navigator.plugins;
+        for (var i = 0; i < plugins.length; i++) {
+            if (plugins[i] && plugins[i].name.toLowerCase().indexOf(name) > -1) {
+                return true;
+            }
+        }
+        return false;
+    },
+    /**
+     * 检测IE浏览器插件是否存在
+     *
+     * @method hasPluginIE
+     * @param {String} name IE浏览器ActiveX插件名称
+     * @return {Boolean} true-插件已经安装;false-未安装
+     */
+    hasPluginIE: function (name) {
+        if (!name)
+            return false;
+        try {
+            new ActiveXObject(name);
+            return true;
+        } catch (ex) {
+            return false;
+        }
+    },
+    /**
+     * 颜色取反，如将白色'#ffffff'转换为黑色'#000000'
+     *
+     * @method colorInverse
+     * @param {String} color 颜色16进制字符表示形式，如：'#ff0000'，表示红色。
+     * @return {String} 取反后的颜色
+     */
+    colorInverse: function (color) {
+        if (color.length < 7) {
+            return '#000000';
+        }
+        var str = color, inverse = "#";
+        for (var i = 1; i < 7; i++) {
+            var s = str.substr(i, 1);
+            inverse += parseInt(15 - parseInt(s, 16)).toString(16);
+        }
+        return inverse;
+    },
+    /**
+     * 获取浏览器语言代码,如:'zh-CN'
+     *
+     * @method getLang
+     * @return {String} 语言代码
+     */
+    getLang: function () {
+        var nav = window.navigator;
+        return (nav.language || nav.userLanguage);
+    },
+    /**
+     * 取消事件冒泡
+     *
+     * @method stopBubble
+     * @param {Object} e 事件对象
+     */
+    stopBubble: function (e) {
+        if (e && e.stopPropagation) {
+            e.stopPropagation();
+        } else {
+            // ie
+            window.event.cancelBubble = true;
+        }
+    },
+    /**
+     * 阻止浏览器默认行为
+     *
+     * @method stopDefault
+     * @param {Object} e 事件对象
+     * @return {Boolean}
+     */
+    preventDefault: function (e) {
+        if (e && e.preventDefault) {
+            e.preventDefault();
+        } else {
+            // ie
+            window.event.returnValue = false;
+        }
+        return false;
+    }
 };
